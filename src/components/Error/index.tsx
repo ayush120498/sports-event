5cbc4583d11c14f1d5c9040d62da3e135bf2ed2e
Ayush Gupta <ashu120498@gmail.com>
2024-01-08 21:16:29 +0530

    feat: Fixed styles

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -2,8 +2,6 @@ import ActionButton from '@Components/Button';
 import './style.scss';
 
 const ErrorComponent = ({ onClick, message = "An error occurred", buttonText = "Reset Page" }: IErrorComponent): JSX.Element => {
-
-
   return <div className='error-container'>
     <h2>{message}</h2>
     <div className='error-container__button'>
