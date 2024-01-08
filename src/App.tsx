119d6f223e4133527722fad849c69ce49a7ad32e
Ayush Gupta <ashu120498@gmail.com>
2024-01-08 14:46:14 +0530

    feat: fix error component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import ErrorComponent from "@Components/Error"
 import { ErrorContext } from "@Context/ErrorContext"
 import ErrorBoundary from "@HOC/ErrorBoundary"
+import withLayout from "@HOC/withLayout/withLayout"
 import Router from "@Routes/Router"
 import { useContext } from "react"
 
+
+const ErrorComponentWithHeader = withLayout(ErrorComponent);
+
 const App = (): JSX.Element => {
 
-
   const errorContext = useContext(ErrorContext);
-
   const handleClick = (): void => {
     errorContext?.updateError(false);
   }
 
-  return <ErrorBoundary fallback={<ErrorComponent onClick={handleClick} />}>
+  return <ErrorBoundary fallback={<ErrorComponentWithHeader onClick={handleClick} />}>
     <Router />
   </ErrorBoundary>
 }
